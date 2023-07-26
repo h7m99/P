11b87956bb69ea6569ba9c6bfcee2bf2ee3661a2
h7m99 <138456646+h7m99@users.noreply.github.com>
2023-07-26 13:25:42 +0400

    Add files via upload

---
+++ timing.py
@@ -0,0 +1,37 @@
+h1 = int(input("what time is it ?"))
+h2 = int(input("what time is it for now ?"))
+
+
+
+m1 = int(input("enter min1"))
+m2 = int(input("enter min2"))
+
+
+
+if h1 > h2 :
+    
+        print("second time comes first")
+
+elif  h2 > h1 :
+    
+       print("first time comes first")
+       
+else:
+     
+    if m1 > m2 :
+        print("second time comes first")
+    
+    elif m2 > m1:
+        print("first time comes first")
+        
+    else :
+        print ("time is the same")
+        
+        
+          
+    
+    
+
+       
+
+
