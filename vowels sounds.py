8e0551e669ba5c3bf869854022791f55a299c89a
h7m99 <138456646+h7m99@users.noreply.github.com>
2023-07-30 09:29:04 +0400

    Add files via upload

---
+++ vowels sounds.py
@@ -0,0 +1,17 @@
+w = input("enter the words : ")
+counter = 0
+for k in w :
+    if k == "o":
+     counter += 1
+    elif k == "u":
+     counter += 1
+    elif k == "a":
+     counter += 1
+    elif k == "i":
+     counter += 1
+    elif k == "e":
+     counter += 1
+    elif k == "y":
+     counter +=1
+print("the number of vowels sounds", counter)
+   
